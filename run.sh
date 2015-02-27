e007b6ad9a9f85759727bd07047ad0fda6fcd99c
mjmcconnell <mitch.mcconnell@mybrighthouse.com>
2015-02-27 17:50:44 -0500

    updated shell script

--- run.sh
+++ run.sh
@@ -1,11 +1,1 @@
-#!/bin/sh
-set -x
-
-case $# in
-	0) echo "usage: run.sh <date> <audit file>";
-	   echo "    Date:  mm/dd/yyyy";exit 1;
-esac
-	   
-groovy AuditTrail.groovy -D $1 -e -f $2 \
-	   -i Activated,Deactivated,Logged,Password,Requested,Granted,For,Email,Feed,Organization *$
-
+groovy AuditTrail.groovy  -D 02/01/2015 -f SetupAuditTrailQA2_02242015.csv -e -w
